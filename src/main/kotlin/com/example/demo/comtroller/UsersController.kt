99106b781e1c5ab8fb3e367d40fbb4a13f198b54
Anna Kim <annajinee@gmail.com>
2018-10-25 16:03:16 +0900

    [수정사항] - 유저추가

---
+++ src/main/kotlin/com/example/demo/comtroller/UsersController.kt
@@ -0,0 +1,47 @@
+package com.example.demo.comtroller
+
+import com.example.demo.model.Users
+import com.example.demo.repository.UsersRepository
+import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UsersController(private val usersRepository: UsersRepository) {
+
+    @PostMapping("/put_user")
+    fun put_user(@RequestBody payload: String): ResponseEntity<*> {
+
+        var reqObj = JSONObject()
+        val parser = JSONParser()
+        val retObj = JSONObject()
+
+        try {
+            val obj = parser.parse(payload)
+            reqObj = obj as JSONObject
+
+            val user = reqObj["user"].toString();
+            val babyName = reqObj["babyName"].toString();
+            val babyWeek = reqObj["babyWeek"].toString();
+            val phoneNum = reqObj["phoneNum"].toString();
+
+            val users = Users()
+            users.user = user
+            users.babyname = babyName
+            users.babyweek = babyWeek
+            users.phonenum = phoneNum
+            usersRepository.save(users)
+            retObj.put("result", "Y")
+
+        } catch (e : Exception){
+            retObj.put("result", "N")
+        }
+
+        return ResponseEntity(retObj.toJSONString(), HttpStatus.OK)
+    }
+
+}
---
+++ src/main/kotlin/com/example/demo/model/Users.kt
@@ -0,0 +1,21 @@
+package com.example.demo.model
+
+import javax.persistence.*
+
+
+@Entity
+@Table(name = "users")
+class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Int = 0
+    @Column(name = "babyname")
+    var babyname: String? = null
+    @Column(name = "babyweek")
+    var babyweek: String? = null
+    @Column(name = "user")
+    var user: String? = null
+    @Column(name = "phonenum")
+    var phonenum: String? = null
+}
